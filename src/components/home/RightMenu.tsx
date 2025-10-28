e3080818840c3d93cd5a01454387d532fea82678
MasterDe <gthinkdesign520@gmail.com>
2025-10-28 10:49:28 +0800

    first commit

---
+++ src/components/home/RightMenu.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import type { FC } from "react";
+import React, { memo } from "react";
+
+const RightMenu: FC = () => {
+  return (
+    <section>
+      <h1>RightMenu</h1>
+    </section>
+  );
+};
+
+export default memo(RightMenu);
